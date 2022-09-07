00ac2455e15eaf7cc4eb07162a1f68d73aa84061
tobnie <niehues.tobias@gmail.com>
2022-09-07 22:26:17 +0200

    package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gaze_predictor_nn',
       version='1.0',
       description='Gaze Predictor Neural Network',
       author='Tobias Niehues',
       author_email='niehues.tobias@gmail.com',
-      packages=['gaze_predictor_nn', 'data'],
+      packages=find_packages(),
       include_package_data=True
 )
