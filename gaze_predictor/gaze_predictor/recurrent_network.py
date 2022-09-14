5c062dc015ce5536495fd944e32014ddd24c3b75
tobnie <niehues.tobias@gmail.com>
2022-09-14 21:16:11 +0200

    new trained models

---
+++ gaze_predictor/gaze_predictor/recurrent_network.py
@@ -0,0 +1,43 @@
+import importlib.resources
+from tensorflow import keras
+from gaze_predictor.gaze_predictor.base_network import NeuralNetwork
+import gaze_predictor.gaze_predictor.data
+
+nn_configuration = {
+    'epochs': 20,  # number of epochs
+    'batch_size': 32,  # size of the batch
+    'verbose': 1,  # set the training phase as verbose
+    'optimizer': keras.optimizers.Adam(clipvalue=1.0),  # optimizer
+    'metrics': ["root_mean_squared_error"],
+    'loss': 'mean_squared_error',  # loss
+    'val_split': 0.2,  # validation split: percentage of the training data used for evaluating the loss function
+    'time_steps': 5,
+    'n_input': 300,
+    'input_shape': (300,),
+    'n_output': 1  # number of outputs = x and y
+}
+
+
+class RecurrentNetwork(NeuralNetwork):
+
+    def __init__(self, name, percent_train=0.8, configuration=None):
+        input_file = 'single_layer_fm_sequence.npz'
+        output_file = 'mfd.npz'
+
+        super().__init__(name, percent_train, configuration)
+        with importlib.resources.path(gaze_predictor.gaze_predictor.data, input_file) as data_path_X:
+            with importlib.resources.path(gaze_predictor.gaze_predictor.data, output_file) as data_path_y:
+                self._load_data(data_path_X, data_path_y, flatten=True)
+
+    # TODO Recurrent Conv1D LSTM natively supported as layer in keras
+    def create_model(self):
+        xavier_initializer = keras.initializers.GlorotUniform()
+        self.model = keras.Sequential([
+            keras.layers.LSTM(64, input_shape=(self.config['time_steps'], self.config['n_input']), name='input',
+                              kernel_initializer=xavier_initializer),
+            keras.layers.Dense(16, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(self.config['n_output'], name='output', kernel_initializer=xavier_initializer)
+        ])
+
+        print(f'Created model for {self.name}:')
+        print(self.model.summary())
