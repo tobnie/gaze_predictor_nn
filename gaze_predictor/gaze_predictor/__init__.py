ff84c5bb94226604a240f99ea3f1fc2436a9974d
tobnie <niehues.tobias@gmail.com>
2022-09-14 16:42:57 +0200

    different FC-Network models

--- gaze_predictor/gaze_predictor/__init__.py
+++ gaze_predictor/gaze_predictor/__init__.py
@@ -1,3 +1,4 @@
-from . import fully_connected
-from . import convolutional
-from . import convolutional_3d
+# from . import fully_connected
+# from . import convolutional
+# from . import convolutional_3d
+from . import *
--- gaze_predictor/gaze_predictor/fully_connected.py
+++ gaze_predictor/gaze_predictor/fully_connected.py
@@ -33,10 +33,97 @@ class FCNetwork(NeuralNetwork):
         self.model = keras.Sequential([
             keras.layers.Dense(self.config['n_input'], input_shape=self.config['input_shape'], name='Input',
                                kernel_initializer=xavier_initializer),
-            keras.layers.Dense(128, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(256, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(16, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(self.config['n_output'], name='Output', kernel_initializer=xavier_initializer)
+        ])
+
+        print(f'Created model for {self.name}:')
+        print(self.model.summary())
+
+
+class FCNetworkDropout(NeuralNetwork):
+
+    def __init__(self, name, percent_train=0.8, configuration=None):
+        input_file = 'single_layer_fm.npz'
+        output_file = 'mfd.npz'
+
+        super().__init__(name, percent_train, configuration)
+        with importlib.resources.path(gaze_predictor.gaze_predictor.data, input_file) as data_path_X:
+            with importlib.resources.path(gaze_predictor.gaze_predictor.data, output_file) as data_path_y:
+                self._load_data(data_path_X, data_path_y, flatten=True)
+
+    def create_model(self):
+        xavier_initializer = keras.initializers.GlorotUniform()
+        self.model = keras.Sequential([
+            keras.layers.Dense(self.config['n_input'], input_shape=self.config['input_shape'], name='Input',
+                               kernel_initializer=xavier_initializer),
+            keras.layers.Dense(256, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
             keras.layers.Dropout(name='DropOut1', rate=0.2),
-            keras.layers.Dense(32, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(16, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
             keras.layers.Dropout(name='DropOut2', rate=0.2),
+            keras.layers.Dense(self.config['n_output'], name='Output', kernel_initializer=xavier_initializer)
+        ])
+
+        print(f'Created model for {self.name}:')
+        print(self.model.summary())
+
+
+class FCNetworkBatchNormalization(NeuralNetwork):
+
+    def __init__(self, name, percent_train=0.8, configuration=None):
+        input_file = 'single_layer_fm.npz'
+        output_file = 'mfd.npz'
+
+        super().__init__(name, percent_train, configuration)
+        with importlib.resources.path(gaze_predictor.gaze_predictor.data, input_file) as data_path_X:
+            with importlib.resources.path(gaze_predictor.gaze_predictor.data, output_file) as data_path_y:
+                self._load_data(data_path_X, data_path_y, flatten=True)
+
+    def create_model(self):
+        xavier_initializer = keras.initializers.GlorotUniform()
+        self.model = keras.Sequential([
+            keras.layers.Dense(self.config['n_input'], input_shape=self.config['input_shape'], name='Input',
+                               kernel_initializer=xavier_initializer),
+            keras.layers.Dense(256, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.BatchNormalization(),
+            keras.layers.Dense(16, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.BatchNormalization(),
+            keras.layers.Dense(self.config['n_output'], name='Output', kernel_initializer=xavier_initializer)
+        ])
+
+        print(f'Created model for {self.name}:')
+        print(self.model.summary())
+
+
+class FCNetworkDataNormalization(NeuralNetwork):
+
+    def __init__(self, name, percent_train=0.8, configuration=None):
+        input_file = 'single_layer_fm.npz'
+        output_file = 'mfd.npz'
+
+        super().__init__(name, percent_train, configuration)
+        with importlib.resources.path(gaze_predictor.gaze_predictor.data, input_file) as data_path_X:
+            with importlib.resources.path(gaze_predictor.gaze_predictor.data, output_file) as data_path_y:
+                self._load_data(data_path_X, data_path_y, flatten=True)
+
+    def create_model(self):
+
+        # normalization of data
+        norm_layer_y = keras.layers.experimental.preprocessing.Normalization()
+        norm_layer_y.adapt(self.y)
+
+        self.y_train = norm_layer_y(self.y_train)
+        self.y_test = norm_layer_y(self.y_test)
+
+        xavier_initializer = keras.initializers.GlorotUniform()
+        self.model = keras.Sequential([
+            keras.layers.Dense(self.config['n_input'], input_shape=self.config['input_shape'], name='Input',
+                               kernel_initializer=xavier_initializer),
+            keras.layers.Dense(256, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.BatchNormalization(),
+            keras.layers.Dense(16, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.BatchNormalization(),
             keras.layers.Dense(self.config['n_output'], name='Output', kernel_initializer=xavier_initializer)
         ])
 
