591d18dd45568efe38403d07322893f05976d4af
tobnie <niehues.tobias@gmail.com>
2022-10-10 13:01:43 +0200

    player position and situations as input

--- gaze_predictor/fully_connected.py
+++ gaze_predictor/fully_connected.py
@@ -1,6 +1,9 @@
+import os
+
+import numpy as np
 from tensorflow import keras
 
-from gaze_predictor.base_network import NeuralNetwork
+from gaze_predictor.base_network import DATA_PATH, NeuralNetwork
 
 nn_configuration = {
     'epochs': 20,  # number of epochs
@@ -32,6 +35,89 @@ class FCNetwork(NeuralNetwork):
         xavier_initializer = keras.initializers.GlorotUniform()
         self.model = keras.Sequential([
             keras.layers.Dense(self.config['n_input'], input_shape=self.config['input_shape'], name='Input',
+                               kernel_initializer=xavier_initializer),
+            keras.layers.Dense(512, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(32, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(self.config['n_output'], name='Output', kernel_initializer=xavier_initializer)
+        ])
+
+        print(f'Created model for {self.name}:')
+        print(self.model.summary())
+
+
+class FCNetworkPlayerPosInput(NeuralNetwork):
+
+    def __init__(self, name, percent_train=0.8, configuration=None, subject_specific=False):
+        input_file = 'player_pos.npz'
+        output_file = 'mfd.npz'
+
+        super().__init__(name, percent_train, configuration)
+
+        self._load_data(input_file, output_file, flatten=True, subject_specific=subject_specific)
+
+    def create_model(self):
+        print('X shape:', self.X.shape)
+
+        xavier_initializer = keras.initializers.GlorotUniform()
+        self.model = keras.Sequential([
+            keras.layers.Dense(self.config['n_input'], input_shape=self.config['input_shape'], name='Input',
+                               kernel_initializer=xavier_initializer),
+            keras.layers.Dense(512, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(32, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
+            keras.layers.Dense(self.config['n_output'], name='Output', kernel_initializer=xavier_initializer)
+        ])
+
+        print(f'Created model for {self.name}:')
+        print(self.model.summary())
+
+
+class FCNetworkSituationInput(NeuralNetwork):
+
+    def __init__(self, name, percent_train=0.8, configuration=None, subject_specific=False, situation_size=7):
+        input_file = f'state_{situation_size}x{situation_size}.npz'
+        output_file = 'mfd.npz'
+
+        super().__init__(name, percent_train, configuration)
+
+        self._load_data(input_file, output_file, flatten=True, subject_specific=subject_specific)
+
+    def _load_subject_data_and_concat(self, input_file, output_file, subject_specific):
+        data_dir = os.getcwd() + DATA_PATH
+        subject_dirs = [f for f in os.listdir(data_dir)]
+
+        subject_X_list = []
+        subject_y_list = []
+        for subject_dir in subject_dirs:
+            input_path = data_dir + subject_dir + '/' + input_file
+            regions_path = data_dir + subject_dir + '/regions.npz'
+            output_path = data_dir + subject_dir + '/' + output_file
+
+            # print(f'Loading from {input_path}...')
+            subject_situation = np.load(input_path)['arr_0']
+            subject_region = np.load(regions_path)['arr_0']
+            subject_X = np.concatenate((subject_situation, subject_region), axis=1)
+
+            # print(f'Loading from {output_path}...')
+            subject_y = np.load(output_path)['arr_0']
+
+            # if only for one subject, immediately return first found subject data
+            if subject_specific:
+                return subject_X, subject_y
+
+            subject_X_list.append(subject_X)
+            subject_y_list.append(subject_y)
+
+        X = np.concatenate(subject_X_list)
+        y = np.concatenate(subject_y_list)
+
+        return X, y
+
+    def create_model(self):
+        print('X shape:', self.X.shape)
+
+        xavier_initializer = keras.initializers.GlorotUniform()
+        self.model = keras.Sequential([
+            keras.layers.Dense(self.X_train.shape[-1], input_shape=(self.X_train.shape[-1],), name='Input',
                                kernel_initializer=xavier_initializer),
             keras.layers.Dense(512, name='Hidden1', activation='relu', kernel_initializer=xavier_initializer),
             keras.layers.Dense(32, name='Hidden2', activation='relu', kernel_initializer=xavier_initializer),
